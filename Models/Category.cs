699d235238031a89b409ead38cea5874a9bf3195
Bryan Makini <bmakini@ymail.com>
2023-03-31 09:35:49 +0100

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Product.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+    }   
+}
+
---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Product.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
